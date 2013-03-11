be08046a27b1c746a68fad8eda7ec2333e06384f
GaetaLab <af342@cornell.edu>
2013-03-11 15:16:27 -0400

    Fixes on OSAS.py
    Signed-off-by: GaetaLab <af342@cornell.edu>

--- instrument/osas.py
+++ instrument/osas.py
@@ -14,26 +14,60 @@ from numpy import savetxt
 from matplotlib import pylab
 
 ## Instrument and address
-instrument_list = {'ANDO': 4}
+instrument_list = {'ANDO': 4,
+                   'HP8563': 18,
+                   'AGILENT': 25,}
 
-addr = instrument_list['ANDO']
+addr = instrument_list['AGILENT']
 
 ## If using Usb2Gpib
 inst = Instrument('GPIB::%d'%addr)
 
+## If using pixlogic
+## Set the right com
+#
+# inst = Instrument('COM6')
+# inst.write('++ %d'%addr)
 
-## ANDO 
-## Read command LDATA, LDATB, LDATC
-##
+def andoAskData():
+    """
+ ANDO 
+ Read command LDATA, LDATB, LDATC
 
-raw_data = inst.ask('LDATA').split(',')
-n = raw_data[0]
-data = [float(i) for i in raw_data[1:]]
+    """
+    raw_data = inst.ask('LDATA').split(',')
+    n = raw_data[0]
+    data = [float(i) for i in raw_data[1:]]
 
-assert len(data) != n
+    assert len(data) != n
+    return data
 
+    
+def Hp8563AskData():
+    """
+ HP Spectrum Analyzer
+ Read command TRA, TRB
+
+    """
+    raw_data = inst.ask('TRA').split(',')
+    n = raw_data[0]
+    data = [float(i) for i in raw_data[1:]]
+
+    assert len(data) != n
+    return data
+
+def AgilentAskData():
+
+    raw_data = inst.ask('TRACE? tra').split(',')
+    n = raw_data[0]
+    data = [float(i) for i in raw_data[1:]]
+
+    assert len(data) != n
+    return data
+    
 ## savefile
+data = AgilentAskData()
 pylab.plot(data)
 pylab.show()
 namefile = raw_input('Save data in file (name) -> ')
-savetxt(namefile + '.txt', data)
+savetxt(namefile + '.txt',data)
