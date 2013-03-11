07f3c91a7f8a1f33294c3b0dd83b2d0080835ac7
GaetaLab <af342@cornell.edu>
2013-03-11 15:16:12 -0400

    Redid counter using simple freq measurement rather than counts
    Signed-off-by: GaetaLab <af342@cornell.edu>

--- instrument/counter.py
+++ instrument/counter.py
@@ -6,26 +6,40 @@ Counts edges for a given time
 from freqtask import FreqTask
 from time import clock,sleep
 from matplotlib.pylab import *
+from matplotlib import animation
 
 ## Parameters
-channels = [2,4] # Channels to measure
-time_d  = 60      # Duration of acquisition in seconds
+sampling_time = 0.001
+time_scale = 4 ## Seconds
 
-tasks = [ FreqTask(ch, 100e3, count= True, debug = True) for ch in channels ]
-counts_t = [ 0 for ch in channels ]
-counts_0 = counts_t[:]
+t = arange(time_scale/sampling_time)
+counts = zeros(t.size)  ## Where the data goes
 
-tic = clock()
-while True:
-    counts_r = [ task.read(1) for task in tasks ]
-    print counts_r
-    counts_t = [ counts_t[i] + (counts_r[i]-counts_0[i]) for i in range(len(channels))]
-    counts_0 = counts_r[:]
+## Init measurement and plot
+task = FreqTask(channel = 2, #channel
+                 sampling_time = sampling_time) # (inverse of sampling rate))
+
+## Create figure
+fig = figure()
+ax = gca()
+line, = plot(counts, 'r-')
+xlabel('Time')
+title('Count rate')
+ylim(0,300000)
+
+## Function in the loop
+def update_counts(n):
+    ## Measurement
+    counts_r = task.read(300)
+    counts[:] = r_[counts[len(counts_r):], counts_r]
     
-    s = "|".join([ " CH%d %8d "%(channels[i], counts_t[i]) for i in range(len(channels))])
-    print s,
-    sleep(0.3)
-    if clock()-tic > time_d: break
-    print "time remaining %3.0f"%(time_d-(clock()-tic))
+    ## Update plot
+    line.set_data(t,counts)
+    fig.canvas.draw()
+    
+## Start the animation, that is a loop with callback 
+line_ani = animation.FuncAnimation(fig, update_counts, repeat = True,
+                                        blit=False)    
+    
 
-input_raw()
+show()
