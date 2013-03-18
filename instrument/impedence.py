20bdcad046fdbe702f8e65489655c3b18236f360
Action Farsi <actionfarsi@gmail.com>
2013-03-18 14:22:01 -0400

    added gui to impedence
    Signed-off-by: Action Farsi <actionfarsi@gmail.com>

--- instrument/impedence.py
+++ instrument/impedence.py
@@ -2,22 +2,33 @@ import visa
 from numpy import *
 from matplotlib.pylab import *
 
-ins = visa.Instrument('com5')
-ins.write('++addr 15')
+from instrument_panel import InstrumentApp
+#ins = visa.Instrument('com5')
+#ins.write('++addr 15')
 
 data = []
 
-ins.write('form4')
-try:
-    ins.write('outpform')
-    while True:
-        data.append( ins.read_values())
-except:
-    pass
+def measure():
+    global data
+    data = []
 
+    ins.write('form4')
+    try:
+        ins.write('outpform')
+        while True:
+            data.append( ins.read_values())
+    except:
+        pass
+        
+    return data
 
-plot(array(data))
-show()
-print 'name file: ',
-name = raw_input()
-savetxt(name + '.txt', array(data))
+def save(filename):
+    savetxt(filename + '.txt', array(data))
+    
+if __name__ == '__main__':
+    app = InstrumentApp(description = "Measure from 8722es")
+    
+    app.addPlotPanel("Measure", measure)
+    app.addValueCtr("Save", save, "temp")
+    
+    app.MainLoop()
