a44a87eaf15f7c71fa06c1b5a0f6a80a4be5487d
actionfarsi <actionfarsi@gmail.com>
2013-05-10 11:43:58 -0400

    Tested with Arduino
    Signed-off-by: actionfarsi <actionfarsi@gmail.com>

--- instrument/detector_firmware/detector_firmware.ino
+++ instrument/detector_firmware/detector_firmware.ino
@@ -13,12 +13,14 @@ const byte DECREASE =  B00001000;  // Reset opcode command
 
 // pins used for the connection with the sensor
 // the other you need are controlled by the SPI library):
-const int TEMP1_PIN = 2;
-const int TEMP2_PIN = 48;
-const int LED1_PIN = 53;
-const int LED2_PIN = 47;
-const int CURRENT1_PIN = 14;
-const int CURRENT2_PIN = 14;
+const int TEMP1_PIN = 3;
+const int TEMP2_PIN = 2;
+const int LED1_PIN = 7;
+const int LED2_PIN = 8;
+const int CURRENT1_PIN = 0;
+const int CURRENT2_PIN = 1;
+
+const double RW = 2;
 
 //Define Variables we'll be connecting to
 double  inputT1, outputT1, setpoint1;
@@ -39,8 +41,10 @@ void setup() {
   // Init the SPI library
   SPI.begin();
   // Configure SPI transmission
-  SPI.setDataMode(1);
+  SPI.setDataMode(SPI_MODE0);
   SPI.setBitOrder(MSBFIRST);
+  digitalWrite(5, LOW);  
+  
   
   // Init and configure PID
   //initialize the variables we're linked to
@@ -55,33 +59,40 @@ void setup() {
   // Configure Leds
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
+
+  pinMode(2, OUTPUT);  
+  pinMode(3, OUTPUT);
 }
+
+
 
 void setResistance(int res, int rn){
    // Convert to n
-   byte n = 256 *(res - RW) / 5000; // 5K Ohm  
+   byte n = (res - 110) / 20; // 5K Ohm  
    if (rn == 0)
      SPI.transfer(WRITE1);
    else
      SPI.transfer(WRITE2);
-   SPI.transfer(n);
+   SPI.transfer(res);
 }
 
 
 
 void loop() {
   Serial.print('R1');
-  setResistance(500, 0);
-  setResistance(5000, 1);
+  analogWrite(2, 0);
+  analogWrite(3, 200);
+  setResistance(150, 0);
+  setResistance(3000, 1);
   digitalWrite(LED1_PIN, HIGH);
   digitalWrite(LED2_PIN, LOW);
-  sleep(10);
+  delay(3000);
   Serial.print('R2');
-  setResistance(500, 1);
-  setResistance(5000, 0);
+  setResistance(300, 0);
+  setResistance(1500, 1);
   digitalWrite(LED1_PIN, LOW);
   digitalWrite(LED2_PIN, HIGH);
-  sleep(10);
+  delay(3000);
   
   if (Serial.available()){
     
