28ce372d54a8088a61bc4cfd8fbb64a7c32b9af6
actionfarsi <actionfarsi@gmail.com>
2013-06-25 15:32:40 -0400

    Improved arduino code
    Signed-off-by: actionfarsi <actionfarsi@gmail.com>

--- instrument/detector_firmware/detector_firmware.ino
+++ instrument/detector_firmware/detector_firmware.ino
@@ -3,7 +3,7 @@
 #include <PID_v1.h>
 #define BUFFER_SIZE 2048
 
-const byte READ1 =     B00001100;  // Read command
+const byte READ  =     B00001100;  // Read command
 const byte WRITE1 =    B00000000;  // Write command
 const byte READ2 =     B00011100;  // Read command
 const byte WRITE2 =    B00010000;  // Write command
@@ -19,7 +19,7 @@ const int LED1_PIN = 6;
 const int LED2_PIN = 7;
 const int CURRENT1_PIN = 2;
 const int CURRENT2_PIN = 3;
-const int COMMMAND_PIN = 4;
+const int CHIPS_PIN = 4;
 
 const double RW = 2;
 
@@ -37,7 +37,7 @@ bool bufferFlag = false;
 
 void setup() { 
   // Init Serial comunication with PC
-  Serial.begin(9800);
+  Serial.begin(14400, SERIAL_8N1);
   
   // Init the SPI library
   SPI.begin();
@@ -63,41 +63,63 @@ void setup() {
 
   pinMode(CURRENT1_PIN, OUTPUT);  
   pinMode(CURRENT1_PIN, OUTPUT);
+  pinMode(CHIPS_PIN, OUTPUT);
 }
-
 
 
 void setResistance(int res, int rn){
    // Convert to n
+   digitalWrite(CHIPS_PIN, LOW);
    byte n = (res - 110) / 20; // 5K Ohm  
    if (rn == 0)
      SPI.transfer(WRITE1);
    else
      SPI.transfer(WRITE2);
    SPI.transfer(res);
+   digitalWrite(CHIPS_PIN, HIGH);
+}
+
+// Read Status of tunable resistor
+void readSPI(int addr = 0){
+    byte b_add = addr << 4;
+    byte n;
+    digitalWrite(CHIPS_PIN, HIGH);
+    Serial.print(addr);
+    Serial.print(" - SPI: ");
+    //Serial.print(READ | b_add, BIN);
+    //Serial.print(" ");
+    n = SPI.transfer(READ | b_add);
+    Serial.print(n, HEX);
+    n = SPI.transfer(NULL);
+    Serial.print(n, HEX);
+    n = SPI.transfer(NULL);
+    Serial.println(n, HEX);
+    digitalWrite(CHIPS_PIN, LOW);
 }
 
 
-
 void loop() {
-  //Serial.print('R1');
+  byte n;
+  Serial.println("R  1");
   //analogWrite(CURRENT1_PIN, 254);
   //analogWrite(CURRENT2_PIN, 0);
   setResistance(150, 0);
   setResistance(3000, 1);
   digitalWrite(LED1_PIN, HIGH);
   digitalWrite(LED2_PIN, LOW);
-  delay(3000);
-  //Serial.print('R2');
+  delay(2000);
+  
+  readSPI(0);
+  readSPI(1);
+  readSPI(2);
+  readSPI(4);
+  readSPI(5);
+ 
+  Serial.println("R  2");
   setResistance(300, 0);
   setResistance(1500, 1);
   digitalWrite(LED1_PIN, LOW);
   digitalWrite(LED2_PIN, HIGH);
-  delay(3000);
-  
-  if (Serial.available()){
-    SPI.transfer(READ1);
-    byte n = SPI.transfer(NULL);
-    Serial.print(n);
-  }
+  delay(2000);
+ 
 }
