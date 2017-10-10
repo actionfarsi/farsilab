b91fbb0e51ad717c64e398c4f89b653e5173c520
Alessandro Farsi <actionfarsi@gmail.com>
2017-10-10 10:44:11 -0400

    Added PID to the temp controlles

--- farsilab/control/devices/tempcontroller/BraggGratingTemp/BraggGratingTemp.ino
+++ farsilab/control/devices/tempcontroller/BraggGratingTemp/BraggGratingTemp.ino
@@ -1,12 +1,16 @@
+#include <PID_v1.h>
+
+
 const int analogInPin[2] = {A0,A1};  // Analog input pin that the potentiometer is attached to
 const int analogOutPin[2] = {9,10}; // Analog output pin that the LED is attached to
 //#define PIN_SWITCH 1
 
 int sensor[2] = {0,0};        // value read from the pot
-float temp[2] = {0,0};        // value output to the PWM (analog out)
-float target[2] = {360,351}; //  
-float delta[2] = {0,0};
-int outputValue[2] = {0,0};
+double temp[2] = {0,0};        // value output to the PWM (analog out)
+double target[2] = {365,310}; //  
+double delta[2] = {0,0};
+double outputValue[2] = {0,0};
+
 // Parametrized curve using beta = 4100K and R25 = 15kOhm and R = 6.8 kOhm
 float _in[] = {103, 129, 159, 193, 231, 273, 318, 366, 415, 465, 515, 563, 610,
        654, 696, 734, 769, 801, 830, 856, 879, 899, 917, 933, 948, 960,
@@ -15,11 +19,26 @@ float _out[] = {270, 274, 278, 283, 287, 292, 296, 301, 305, 310, 314, 319, 323,
        328, 332, 337, 341, 346, 350, 355, 359, 364, 368, 373, 377, 382,
        386, 391, 395, 400};
 
+double Kp=70, Ki=1, Kd=20;
+PID PID1(&temp[0], &outputValue[0], &target[0], Kp, Ki, Kd, DIRECT);
+PID PID2(&temp[1], &outputValue[1], &target[1], Kp, Ki, Kd, DIRECT);
+
+
 void setup() {
   // put your setup code here, to run once:
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-  
+
+  // read the analog in value:
+  sensor[0] = analogRead(analogInPin[0]);
+  sensor[1] = analogRead(analogInPin[1]);
+
+  // Setpoints
+  double target[2] = {365,310};
+
+  //turn the PID on
+  PID1.SetMode(AUTOMATIC);
+  PID2.SetMode(AUTOMATIC);
 }
 
 void loop() {
@@ -45,23 +64,26 @@ void loop() {
   
   delta[0] = temp[0]-target[0]; 
   delta[1] = temp[1]-target[1]; 
+
+  PID1.Compute();
+  PID2.Compute();
   
-  if (delta[0] > 0){
-    analogWrite(analogOutPin[0],00 );
-    Serial.println("\tH0 OFF ");
-  } else {
-    outputValue[0] = map(delta[0], 0, 1023, 0, 254);
-    analogWrite(analogOutPin[0], 254);
+  //if (delta[0] > 0){
+  //  analogWrite(analogOutPin[0], 0 );
+  //  Serial.print("\tH0 OFF ");
+  //} else {
+    analogWrite(analogOutPin[0], outputValue[0]);
+    Serial.print(outputValue[0]);
     Serial.println("\tH0 On "); 
-  }
-  if (delta[1] > 0){
-    analogWrite(analogOutPin[1],00 );
-    Serial.println("\tH1 OFF ");
-  } else {
-    outputValue[1] = map(delta[1], 0, 1023, 0, 254);
-    analogWrite(analogOutPin[1], 254);
+  //}
+  //if (delta[1] > 0){
+  //  analogWrite(analogOutPin[1],00 );
+  //  Serial.println("\tH1 OFF ");
+  //}  else {
+    analogWrite(analogOutPin[1], outputValue[1]);
+    Serial.print(outputValue[1]);
     Serial.println("\tH1 On "); 
-  }
+  //}
 }
 
 float FmultiMap(float val, float * _in, float * _out, uint8_t size)
