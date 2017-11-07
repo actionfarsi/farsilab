26ae6cee5adb5c604f6595b1952b937a2d6ff119
Alessandro Farsi <actionfarsi@gmail.com>
2017-11-07 17:17:32 -0500

    Added load script in __init__

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from farsilab import *
--- farsilab/__init__.py
+++ farsilab/__init__.py
@@ -1,1 +1,5 @@
-""" Utils submodule """
+""" Utils submodules """
+from .units import Q_, ureg
+from .braggscattering import k_bs, BS_evolution, BS_mc_evolution
+from .braggscattering import plot_mc_results, optimal_bs
+from .dispersion import Dispersion
